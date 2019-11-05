3303f60b7ee918a936539d2436b8bdf15c14e89a
donutwizard666 <thebeardedwarrior@icloud.com>
2019-11-05 12:35:53 -0800

    set up component structure for the application

---
+++ reducer-todo/src/reducers/reducer.jsx
@@ -0,0 +1,14 @@
+import React, { useReducer } from 'react';
+
+export const initialState = {
+  item: 'Learn About Reducers',
+  completed: false,
+  id: 3849547372
+}
+
+export function reducer(state, action) {
+  
+  return {
+    
+  }
+}
