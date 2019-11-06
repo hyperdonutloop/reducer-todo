3c946f3b8c112017c99f726a9bb1992d959b6866
donutwizard666 <thebeardedwarrior@icloud.com>
2019-11-05 20:47:56 -0800

    created simple form component with an Add Task button

--- reducer-todo/src/components/TodoForm.jsx
+++ reducer-todo/src/components/TodoForm.jsx
@@ -1,2 +1,21 @@
+import React from 'react';
 
+const TodoForm = () => {
+
+  return (
+    <div className="tasks">
+     <form>
+       <label htmlFor="item">New Task</label>
+       <input
+        type="text"
+        name="item"
+        id="item"
+       />
+       <button>Add Task!</button>
+     </form>
+    </div>
+  )
+}
+
+export default TodoForm;
 
