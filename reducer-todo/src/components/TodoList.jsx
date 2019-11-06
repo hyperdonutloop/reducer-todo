e20adbc875278b34ab2ffc62fd3e1817fa1484ab
donutwizard666 <thebeardedwarrior@icloud.com>
2019-11-05 21:54:28 -0800

    tried to add in adding, removing, and toggle completed functions

--- reducer-todo/src/components/TodoList.jsx
+++ reducer-todo/src/components/TodoList.jsx
@@ -4,7 +4,8 @@ import { reducer, initialState } from '../reducers/reducer';
 const TodoList = () => {
   const [ state, dispatch ] = useReducer(reducer, initialState);
   const [ item, setItem ] = useState();
-
+  console.log(state);
+  
   return (
     <div className="todo-list">
       <h1>Todo List</h1>
