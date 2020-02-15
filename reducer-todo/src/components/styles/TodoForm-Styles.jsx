b3544690e198a91a6e0b5672d0b0c7d1a9870f0b
donutwizard666 <thebeardedwarrior@icloud.com>
2020-02-14 23:00:08 -0800

    removed green border

--- reducer-todo/src/components/styles/TodoForm-Styles.jsx
+++ reducer-todo/src/components/styles/TodoForm-Styles.jsx
@@ -8,7 +8,7 @@ export const Wrapper = styled.div`
       display: flex;
       flex-direction: column;
       justify-content: center;
-      border: 2px solid green;
+      /* border: 2px solid green; */
     }
   }
 
